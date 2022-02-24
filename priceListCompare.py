bfcbb82d36768f865dc40ccd01bbe23ec9ffb498
milad <milad@hey.com>
2022-02-24 06:11:33 -0800

    pseudocode

--- priceListCompare.py
+++ priceListCompare.py
@@ -30,6 +30,20 @@ import numpy as np
 #  XXXXX        HIGHER    VIZIENT 2016 PRICE LIST          $100                 $150
 
 
+
+#--------------------------------------------USE CASE 1---------------------------------------------------------------------
+#Summary:Compare prior qt us master pricing to current qt
+#Input:Prior qt pricing & current qt pricing
+#Output:All part numbers pricing delta, status (no change, price list increase, price list decreased, new, deleted).
+
+#Definition of status:
+#   No Change: no pricing changes compared to last qt
+#   Price List Increase: Price list is higher compared to last qt
+#   Price list decrease: Price list is lower compared to last qt
+#   New: Part number not found compared to last qt
+#   Deleted: Part number was found in obsolete file 
+
+
 #CREATE DF FOR PRIOR QT US MASTER
 prior_US_Master = 'files/2021_Q4_US_Master.xlsx'
 df_old_US_Master = pd.read_excel(prior_US_Master)
@@ -39,6 +53,34 @@ df_old_US_Master = pd.read_excel(prior_US_Master)
 curr_US_Master = 'files/2021_Q4_US_Master.xlsx'
 df_curr_US_Master = pd.read_excel(curr_US_Master)
 
+#COPY PART NUMBERS FROM CURRENT US MASTER INTO NEW FILE
+#PART NUMBER | DESCRIPTION | HIERARCHYY (MAG, AG, BU) | OLD PRICE LIST | NEW PRICE LIST | DELTA | STATUS 
+
+
+#IF PART NUMBER NOT FOUND, SET AS NEW STAUS AND PULL IN DATA FROM CURRENT US MASTER INTO SHEET
+
+#IF PART NUMBER IS FOUND, POPULATE OLD PRICE, NEW PRICE LIST
+#CALCULATE DELTA
+#SET STATUS BASED ON DEFINITION
+
+
+
+#
+
+
+#
+
+
+#
+
+print(df_old_US_Master)
+
+#
+
+#--------------------------------------------USE CASE 2---------------------------------------------------------------------
+#Summary:
+#Input:
+#Output:
 
 #CREATE DF FOR ALL CUSTOMER PRICE BOOKS
 
@@ -47,4 +89,4 @@ df_curr_US_Master = pd.read_excel(curr_US_Master)
 
 #SAVE RESULTS TO A NEW EXCEL FILE 
 
-print(df_old_US_Master)
+
